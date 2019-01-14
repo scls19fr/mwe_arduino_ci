4a2a139bdb81cdcb36c078b4345e044267743f1b
Sébastien Celles <s.celles@gmail.com>
2019-01-14 21:02:01 +0100

    Order

---
+++ test/test_mwe_02.cpp
@@ -0,0 +1,16 @@
+#include <ArduinoUnitTests.h>
+
+
+//#include <Arduino.h>
+
+//#include "types.h"
+//#include "laptimer.h"
+//#include "debug.h"
+//#include "config.h"
+
+unittest(unittest_mwe)
+{
+  assertTrue(true);
+}
+
+unittest_main()
