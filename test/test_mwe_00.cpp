94b8703f5f294d67b42fc6dceef5475a142b3770
Sébastien Celles <s.celles@gmail.com>
2019-01-14 21:09:06 +0100

    Remove unused include

--- test/test_mwe_00.cpp
+++ test/test_mwe_00.cpp
@@ -1,13 +1,5 @@
 #include <ArduinoUnitTests.h>
 
-
-//#include <Arduino.h>
-
-//#include "types.h"
-//#include "laptimer.h"
-//#include "debug.h"
-//#include "config.h"
-
 unittest(unittest_mwe)
 {
   assertTrue(true);
